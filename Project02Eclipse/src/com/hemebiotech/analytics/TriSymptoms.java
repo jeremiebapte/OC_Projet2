aa9a27b2b069ade590b998edfe928671793a35ef
Jérémie Bapté <jeremiebapte89@gmail.com>
2021-06-24 21:35:54 +0200

    fonction tri

--- Project02Eclipse/src/com/hemebiotech/analytics/TriSymptoms.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/TriSymptoms.java
@@ -1,26 +1,12 @@
 package com.hemebiotech.analytics;
 
 import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class TriSymptoms {
 
-    // lecture et tri des symptomes.
-    public TreeMap<String, Integer> sortingSymptomsData() {
-
-        ReadSymptomDataFromFile file = new ReadSymptomDataFromFile("symptoms.txt");
-        file.getSymptoms();
-        List<String> symptomsFile = file.getSymptoms();
-
-        TreeMap<String, Integer> symptomsTri = new TreeMap<>();
-        for (String listSymptoms : symptomsFile) {
-            if (symptomsTri.containsKey(listSymptoms)) {
-                symptomsTri.put(listSymptoms, symptomsTri.get(listSymptoms) + 1);
-            } else {
-                symptomsTri.put(listSymptoms, 1);
-            }
-
-        }
-        return symptomsTri;
+    public Map<String,Integer> sortSymptoms(Map<String,Integer> symptoms) {
+        return new TreeMap<>(symptoms);
     }
 }

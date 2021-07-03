bf26e779dd9b52120053f48214443e7c4bcff471
jeremiebapte <jeremiebapte89@gmail.com>
2021-07-03 18:49:04 +0200

    fonction tri symptomes et mise en map

--- Project02Eclipse/src/com/hemebiotech/analytics/TriSymptoms.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/TriSymptoms.java
@@ -3,7 +3,10 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
+/*
+* Sorting of symptoms and putting in Map.
+*
+* */
 public class TriSymptoms {
 
     public Map<String,Integer> sortSymptoms(Map<String,Integer> symptoms) {

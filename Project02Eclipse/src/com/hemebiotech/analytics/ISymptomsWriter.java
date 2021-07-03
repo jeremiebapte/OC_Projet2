c54909ad3f7e5f07d7b3ccd5471f2efaeaa2eab6
jeremiebapte <jeremiebapte89@gmail.com>
2021-07-03 18:46:21 +0200

    interface écriture

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomsWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomsWriter.java
@@ -1,7 +1,9 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
-
+/*
+*  Interface implements the write function
+* */
 public interface ISymptomsWriter {
     public void writeSymptomsData(Map<String,Integer> sortingSymptomsData);
 

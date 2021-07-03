82228efef12f37aa745a836bb13af32332dbdcd8
jeremiebapte <jeremiebapte89@gmail.com>
2021-07-03 18:33:21 +0200

    fonction ecriture dans fichier result.

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptoms.java
@@ -0,0 +1,40 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class WriteSymptoms implements ISymptomsWriter {
+    /*tri et écriture dans le fichier result.out
+     *
+     * */
+    @Override
+    public void writeSymptomsData(Map<String,Integer> sortingSymptomsData) {
+
+        FileWriter writer = null;
+        try {
+
+
+            writer = new FileWriter("result.out");
+            for (Map.Entry<String, Integer> entree : sortingSymptomsData.entrySet()){
+                writer.write(entree.getKey() + " : " + entree.getValue() + "\n");
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'écriture du fichier");
+
+        }finally {
+            if(writer!=null){
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("Erreur lors de la fermeture du fichier");
+                }
+            }
+        }
+
+    }
+
+}

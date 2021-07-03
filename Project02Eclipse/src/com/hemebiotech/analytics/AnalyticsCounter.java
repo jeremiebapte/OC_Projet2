39168337a161df68e02597a8d101e81cc1168fa6
jeremiebapte <jeremiebapte89@gmail.com>
2021-07-03 18:38:21 +0200

    fonction main avec résultat final

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,36 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	/* TreeMap avec symptomes triés.
+	 * Affichage console.
+	 *
+	 * */
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+	public static void main(String[] args) throws IOException {
+
+		ReaderSymptom finalReader = new ReaderSymptom();
+		List<String> finalSymptoms = finalReader.readSymptoms();
+
+		CountSymptoms finalCount = new CountSymptoms();
+		Map<String,Integer> countedSymptoms = finalCount.countingSymptomsData(finalSymptoms);
+
+		TriSymptoms finalSort = new TriSymptoms();
+		Map<String,Integer> sortedSymptoms = finalSort.sortSymptoms(countedSymptoms);
+
+		ISymptomsWriter finalResult = new WriteSymptoms();
+		finalResult.writeSymptomsData(sortedSymptoms);
+
+
+
+		for (Map.Entry<String, Integer> entree : sortedSymptoms.entrySet())
+			System.out.println("Symptoms : " + entree.getKey() + ": " +entree.getValue());
+
+
 	}
+
 }

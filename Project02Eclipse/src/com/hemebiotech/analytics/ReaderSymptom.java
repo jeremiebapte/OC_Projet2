bbaaff83db26d222d3cd513f26f1bcaaa3cc626b
jeremiebapte <jeremiebapte89@gmail.com>
2021-07-03 18:47:43 +0200

    fonction lecture symptomes et mise en liste

--- Project02Eclipse/src/com/hemebiotech/analytics/ReaderSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReaderSymptom.java
@@ -3,8 +3,9 @@ package com.hemebiotech.analytics;
 import java.util.List;
 
 public class ReaderSymptom {
-    /* Lecture fichier symptomes
-     *Mise en Liste
+    /*
+     *Reading symptom file
+     * Listing
      */
     public List<String> readSymptoms(){
         ISymptomReader file = new ReadSymptomDataFromFile("symptoms.txt");
